ed511b0d1c36bd68b3e2edf3b974234f03d505cd
DmitriyFokin <fokinds1@gmail.com>
2018-03-12 13:59:16 +0300

    add insert vector rand numbers

---
+++ KruglovTaskVS5/KruglovTaskVS5.cpp
@@ -0,0 +1,61 @@
+// KruglovTaskVS5.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+typedef struct DataStruct
+{
+	int key1;
+	int key2;
+	string  str;
+};
+
+vector<DataStruct> addStruct(int amountOfElements)
+{
+	vector<DataStruct> vStruct;
+	DataStruct temp_struct;
+	string stringArr[10] = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+	int temp_key1, temp_key2;
+	string temp_str;
+	srand(time(0));
+
+	for (int i = 0; i < amountOfElements; i++)
+	{
+		temp_key1 = (rand() % 11) - 5;
+		temp_key2 = (rand() % 11) - 5;
+		temp_str = stringArr[rand() % 10];
+		temp_struct.key1 = temp_key1;
+		temp_struct.key2 = temp_key2;
+		temp_struct.str = temp_str;
+		vStruct.push_back(temp_struct);
+	}
+
+	return vStruct;
+}
+
+void show(vector<DataStruct> &vec)
+{
+	for (int i = 0; i < vec.size(); i++)
+		cout << i << ". Str: " << vec[i].str << ".\t Key1: " << vec[i].key1 << ".\t Key2: " << vec[i].key2 << endl;
+}
+
+int main()
+{
+	vector<DataStruct> vectorMain;
+	vector<DataStruct>::iterator first, last;
+
+
+	vectorMain = addStruct(10);
+	first = vectorMain.begin();
+	last = vectorMain.end();
+	show(vectorMain);
+	
+	//stable_sort(first, last);
+	//for_each(first, last, cout << i)
+
+	_getch();
+    return 0;
+}
+
---
+++ KruglovTaskVS5/stdafx.h
@@ -0,0 +1,19 @@
+// stdafx.h: включаемый файл для стандартных системных включаемых файлов
+// или включаемых файлов для конкретного проекта, которые часто используются, но
+// не часто изменяются
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+// TODO: Установите здесь ссылки на дополнительные заголовки, требующиеся для программы
+#include <vector>
+#include <algorithm>
+#include <conio.h>
+#include <string>
+#include <iostream>
+#include <ctime>
